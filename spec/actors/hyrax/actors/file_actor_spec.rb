2eee8215b4e3a6ed5d605bc28d1d29ad5d7bd62b
Adam Ploshay <aploshay@iu.edu>
2018-09-11 08:33:04 -0400

    [HPT-1393] Replace IngestFileJob with overriden FileActor

--- spec/actors/hyrax/actors/file_actor_spec.rb
+++ spec/actors/hyrax/actors/file_actor_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 require 'rails_helper'
 
-describe IngestFileJob do
+describe Hyrax::Actors::FileActor do
   let(:file_set) { FactoryBot.create(:file_set) }
   let(:filename) { Rails.root.join("spec", "fixtures", "world.png").to_s }
   let(:user)     { FactoryBot.create(:user) }
+  let(:file_actor) { described_class.new(file_set, :original_file, user) }
+  let(:file) { File.new(filename) }
+  let(:io) { JobIoWrapper.create_with_varied_file_handling!(user: user, file: file, relation: :original_file, file_set: file_set) }
 
   context 'when :store_original_files is false', :clean do
     it 'sets the mime_type to an external body redirect' do
-      expect(CreateDerivativesJob).to receive(:perform_now) \
-        .with(file_set, String, String)
+      expect(CharacterizeJob).not_to receive(:perform_later)
       allow(Catorax.config).to receive(:[]) \
         .with(:store_original_files) \
         .and_return(false)
@@ -22,8 +24,7 @@ describe IngestFileJob do
       allow(Catorax.config).to receive(:[]) \
         .with(:master_file_service_url) \
         .and_return('http://service')
-      described_class.perform_now(file_set, filename, user,
-                                  mime_type: 'image/png')
+      file_actor.ingest_file(io)
       expect(file_set.reload.original_file.mime_type).to include \
         "#{Catorax.config[:master_file_service_url]}"
     end
@@ -32,7 +33,7 @@ describe IngestFileJob do
   context 'when :store_original_files is true', :clean do
     it 'saves an image file to the member file_set' do
       expect(CharacterizeJob).to receive(:perform_later) \
-        .with(file_set, String)
+        .with(file_set, String, String)
       allow(Catorax.config).to receive(:[]) \
         .with(:store_original_files) \
         .and_return(true)
@@ -42,8 +43,7 @@ describe IngestFileJob do
       allow(Catorax.config).to receive(:[]) \
         .with(:index_hocr_files) \
         .and_return(true)
-      described_class.perform_now(file_set, filename, user,
-                                  mime_type: 'image/png')
+      file_actor.ingest_file(io)
       expect(file_set.reload.original_file.mime_type).to include "image/png"
     end
   end
